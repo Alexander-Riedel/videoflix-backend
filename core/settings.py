3ee5e732894a3058bbd2dc627d3e7f939080ce96
Alexander Riedel <alexander.riedel@riedelmedia.de>
2025-08-02 19:46:55 +0200

    Update .gitignore, settings.py, and docker-compose.yml to enhance configuration and service definitions

--- core/settings.py
+++ core/settings.py
@@ -43,8 +43,8 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200', 
     'http://127.0.0.1:4200', 
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
+    'http://localhost:5500',
+    'http://127.0.0.1:5500',
 ]
 
 CORS_ALLOW_CREDENTIALS = True
